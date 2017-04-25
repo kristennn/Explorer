95b56cc3aa5cac32276178c1af350f538109851d
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-04-26 07:15:21 +0800

    play CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-puts ""
-Job.create!(title: "", description: "", wage_upper_bound: rand(6000..12000), wage_lower_bound: rand(3500..6000), contact_email: "#{i}*1002#{i}*200.com")
-end
