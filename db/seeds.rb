309cc395c7106a011053d1d61a727060a565cf9d
郑笑飞“git config --global user.email 13949836783@163.com” <zhengxiaofei@zhengxiaofeideMacBook-Air.local>
2017-04-28 09:45:38 +0800

    add character

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,9 @@ puts "这个seeds文件可以自动建立一个admin账号"
 create_account = User.create([email: '1181522516@qq.com', password:'123456', password_confirmation: '123456', is_admin: 'true'])
 
 puts "Admin account created."
+
+puts "这个seeds文件可以自动建立16种人格"
+
+Character.create!([name: "ESTJ"])
+
+puts "16 Characters created"
